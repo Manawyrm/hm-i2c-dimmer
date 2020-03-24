b0d7d46eecd83c9c42203b977d953aa26db1897b
Tobias Mädel <t.maedel@alfeld.de>
2020-03-24 19:43:24 +0100

    Add GPL license header

--- src/hm-i2c-dimmer.c
+++ src/hm-i2c-dimmer.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020 Tobias Schramm <t.schramm@manjaro.org>
+ * Copyright (C) 2020 Tobias Mädel <t.maedel@alfeld.de>
+ *
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
