5546b3628b88d38338d074b3dcfe5c7b8a2e6c59
Tobias Mädel <t.maedel@alfeld.de>
2020-03-24 18:30:43 +0100

    Add i2c control
    single byte writes for brightness setting
    
    register address = channel

--- src/hm-i2c-dimmer.c
+++ src/hm-i2c-dimmer.c
@@ -2,9 +2,22 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/i2c.h>
 
 #define BASE_PERIOD_LEN 100
 #define PWM_CHANNEL_COUNT 2
+
+#define I2C_ADDRESS 0x32
+
+typedef enum
+{
+	I2C_START, 
+	I2C_REGISTER_SET,
+	I2C_STOP,
+} i2c_state_t;
+
+i2c_state_t i2c_state = I2C_STOP;
+uint8_t i2c_register = 0x00;
 
 typedef struct
 {
@@ -18,15 +31,41 @@ pwm_chan_t pwm_channels[PWM_CHANNEL_COUNT] = {
 	{GPIOA, GPIO6, 0},
 };
 
+static void i2c_slave_init(uint8_t slave_address)
+{
+	nvic_enable_irq(NVIC_I2C1_EV_IRQ);
+
+	// configure i2c pins
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+			 GPIO_I2C1_SDA); //PB7
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+			 GPIO_I2C1_SCL); //PB6
+
+	i2c_reset(I2C1);	
+	i2c_peripheral_disable(I2C1);
+
+	i2c_set_speed(I2C1, i2c_speed_sm_100k, I2C_CR2_FREQ_36MHZ);
+	i2c_set_own_7bit_slave_address(I2C1, slave_address);
+	i2c_enable_interrupt(I2C1, I2C_CR2_ITEVTEN | I2C_CR2_ITBUFEN);
+	i2c_peripheral_enable(I2C1);
+
+	// slave needs to acknowledge on receiving bytes
+	// set it after enabling Peripheral i.e. PE = 1
+	i2c_enable_ack(I2C1);
+}
+
 static void clock_setup(void)
 {
-	/* Set STM32 to 72 MHz. */
+	// Set STM32 to 72 MHz.
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
-	/* Enable GPIO A-C clock. */
+	// Enable GPIO A-C clock.
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_GPIOC);
+
+	// Enable I2C clock.
+	rcc_periph_clock_enable(RCC_I2C1);
 }
 
 static void gpio_setup(void)
@@ -105,21 +144,84 @@ void tim2_isr(void)
 	}
 }
 
+void i2c1_ev_isr(void)
+{
+	uint32_t sr1, sr2;
+
+	sr1 = I2C_SR1(I2C1);
+
+	// Address matched (Slave)
+	if (sr1 & I2C_SR1_ADDR)
+	{
+		// Clear the ADDR sequence by reading SR2.
+		i2c_state = I2C_START; 
+		sr2 = I2C_SR2(I2C1);
+		(void) sr2;
+	}
+	// Receive buffer not empty
+	else if (sr1 & I2C_SR1_RxNE)
+	{
+		// Read register address
+		if (i2c_state == I2C_START)
+		{
+			uint8_t tmp = i2c_get_data(I2C1);
+			if (tmp < PWM_CHANNEL_COUNT)
+			{
+				i2c_register = tmp; 
+				i2c_state = I2C_REGISTER_SET;
+			}
+			else
+			{
+				i2c_state = I2C_STOP;
+			}
+		}
+		else if (i2c_state == I2C_REGISTER_SET)
+		{
+			pwm_channels[i2c_register].value = i2c_get_data(I2C1);
+		} 
+		else
+		{
+			// discard data
+			i2c_get_data(I2C1);
+		}
+	}
+	// Transmit buffer empty & Data byte transfer not finished
+	else if ((sr1 & I2C_SR1_TxE) && !(sr1 & I2C_SR1_BTF))
+	{
+		if (i2c_state == I2C_START)
+		{
+			i2c_send_data(I2C1, pwm_channels[i2c_register].value);
+		}
+	}
+	// done by master by sending STOP
+	//this event happens when slave is in Recv mode at the end of communication
+	else if (sr1 & I2C_SR1_STOPF)
+	{
+		i2c_peripheral_enable(I2C1);
+		i2c_state = I2C_STOP;
+	}
+	//this event happens when slave is in transmit mode at the end of communication
+	else if (sr1 & I2C_SR1_AF)
+	{
+		I2C_SR1(I2C1) &= ~(I2C_SR1_AF);
+	}
+}
 
 int main(void)
 {
 	clock_setup();
 	gpio_setup();
 	timer_setup();
+	i2c_slave_init(I2C_ADDRESS);
 
 	while (1) {
 		for (int i = 0; i < 1000000; i++)	/* Wait a bit. */
 			__asm__("nop");
 		
-		for (int i = 0; i < PWM_CHANNEL_COUNT; ++i)
+		/*for (int i = 0; i < PWM_CHANNEL_COUNT; ++i)
 		{
 			pwm_channels[i].value++;
-		}
+		}*/
 	}
 
 	return 0;
